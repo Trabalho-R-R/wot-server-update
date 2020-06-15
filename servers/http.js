4d53899d871d7e6568457c44238155c0a4039fdb
rmsc27 <59347723+rmsc27@users.noreply.github.com>
2020-06-15 23:36:44 +0100

    Update http.js

--- servers/http.js
+++ servers/http.js
@@ -25,34 +25,3 @@ app.get('/pi', function (req, res) {
 // For representation design
 //app.use(converter());
 module.exports = app;
-
-
-/*
- //Initial version:
-
-var express = require('express'),
-  actuatorsRoutes = require('./../routes/actuators'),
-  sensorRoutes = require('./../routes/sensors'),
-  resources = require('./../resources/model'), //#A
-  cors = require('cors'); 
-
-var app = express(); //#B
-
-app.use(cors()); //#C
-
-app.use('/pi/actuators', actuatorsRoutes); //#D
-app.use('/pi/sensors', sensorRoutes);
-
-app.get('/pi', function (req, res) { //#E
-  res.send('This is the WoT-Pi!')
-});
-
-module.exports = app;
-
-//#A Requires the Express framework, your routes, and the model
-//#B Creates an application with the Express framework; this wraps an HTTP server
-//#C Enable CORS support (see section 6.1.5)
-//#D Binds your routes to the Express application; bind them to /pi/actuators/... and /pi/sensors/...
-//#E Create a default route for /pi
-
-*/
